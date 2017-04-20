c529c9796e820305340268312ba1255ab9b78fa3
jackiehanna <jackie.hanna@nyu.edu>
2017-04-19 22:20:32 -0400

    Add files via upload

---
+++ testing typing game.py
@@ -0,0 +1,14 @@
+print "Welcome to the typing game!"
+print "Please type your name."
+name = raw_input ("My name is ")
+
+print "Welcome, " + str(name) + "! Let's start out easy."
+print "Type the words you see: "
+hellotest = raw_input ("hello ")
+if hellotest != "hello":
+    print "Nope. Try again!"
+    hellotest = raw_input ("hello ")
+
+print "Good job!"
+    
+    
